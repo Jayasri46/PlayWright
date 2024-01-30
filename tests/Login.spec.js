08154e538a507c6333741bdee6236dacb5631b47
Yamini@1146 <Yamini@1146>
2024-01-30 11:06:26 +0530

    Playwright

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,14 +1,15 @@
 const {test,expect}=require('@playwright/test')
-test('Login Functionlity',async({page})=>{
-   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    //await page.click()
-    await expect(page).toHaveTitle('OrangeHRM');
-    await page.locator("//input[@name='username']").fill('Admin');
-    await page.locator("//input[@name='password']").type('admin123');
-    await page.click("//button[@type='submit']")
-    await page.close();
-}
-)
+test.only('Login Functionlity',async({page})=>{
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        await expect(page).toHaveTitle('OrangeHRM');
+        await page.locator("//input[@name='username']").click();
+        await page.locator("//input[@name='username']").fill('Admin');
+        await page.locator("//input[@name='password']").click();
+        await page.locator("//input[@name='password']").type('admin123');
+        await page.click("//button[@type='submit']")
+        await page.close();
+    }
+    )
 
 
 
