e82af84212e6a2586c26f598a35cebb523d89584
Yamini@1146 <Yamini@1146>
2024-03-01 13:42:23 +0530

    Updated Assignment

--- Concat.js
+++ Concat.js
@@ -1,35 +1,35 @@
 //concatenating Numbers
-const n1=4;
-const n2=6;
-const res=n1+n2;
-console.log("Number concatenation:",res);
+const number1=4;
+const number2=6;
+const result=number1+number2;
+console.log("Number concatenation:",result);
 
 //concatenating Strings
-const s1='Welcome to'
-const s2='PalTech'
-const sr=s1+s2;
-console.log("String Concatenation:",sr);
+const string1='Welcome to'
+const string2='PalTech'
+const stringresult=string1+string2;
+console.log("String Concatenation:",stringresult);
 
 //concatenating booleans
-const b1=true
-const b2=false
-const br=b1+b2
-console.log("Boolean concatenation:",br);
+const boolean1=true
+const boolean2=false
+const booleanresult=boolean1+boolean2
+console.log("Boolean concatenation:",booleanresult);
 
 //Mixed combinations 
-const n=3;
-const s='Jayasri';
-const b=true;
-const m=n+s+b;
-console.log("Mixed result:",m);
+const number=3;
+const string='Jayasri';
+const boolean=true;
+const mixedresult=number+string+boolean;
+console.log("Mixed result:",mixedresult);
 
 //Numbers and Strings
-const mr=n1+s1;
-console.log("Mixed result1:",mr);
+const mixedresult1=number1+string1;
+console.log("Mixed result1:",mixedresult1);
 
 //boolean and string
-const bs=b1+s2
-console.log("Mixed result2:",bs);
+const booleanstring1=boolean1+string2
+console.log("Mixed result2:",booleanstring1);
 
 // Using identity operator
 const boolString=true
@@ -37,17 +37,17 @@ console.log((boolString === "true"));
 const boolValue = (boolString === "true"); 
 console.log(boolValue); 
 
-const v1 = !true;
-const v2 = !'';
+const version1 = !true;
+const version2 = !'';
 
-console.log(v1);
-console.log(v2); 
+console.log(version1);
+console.log(version2); 
 
-const v3=!!true;
-const v4=!!'';
+const version3=!!true;
+const version4=!!'';
 
-console.log(v3);
-console.log(v4); 
+console.log(version3);
+console.log(version4); 
 
 // Using Regex
 console.log((/true/).test(boolString)); 
